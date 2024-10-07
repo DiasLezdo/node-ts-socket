067c2ffa39bac713141b780b62440083bc847c5f
DiazMaria <diazlezdo@gmail.com>
2024-10-07 13:01:10 +0530

    initiate the server

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+// Import the 'express' module
+import express from "express";
+
+// Create an Express application
+const app = express();
+
+// Set the port number for the server
+const port = 5000;
+
+// Define a route for the root path ('/')
+app.get("/", (req, res) => {
+  // Send a response to the client
+  res.send("Hello, TypeScript + Node.js + Express!");
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  // Log a message when the server is successfully running
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+// --------------------------- @ @ @ @ ------------------------------------
+
+// steps --> based our implementation
+
+// npm run build ---> build our project and change or compile to js
+
+// npm start -->dist/app.js
